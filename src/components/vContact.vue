66642eaccb0888ff234d2e10da5ed3653526da70
mipaifu328 <594757223@qq.com>
2018-07-31 17:35:29 +0800

    css->sass

--- src/components/vContact.vue
+++ src/components/vContact.vue
@@ -48,50 +48,49 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .contact-container{
     max-width: 300px;
     margin: 0 auto;
     text-align: center;
-  }
-  .contact-des1{
-    margin: 0 auto;
-    display: flex;
-    display: flex;
-    justify-content: space-between;
-    font-size: 22px;
-  }
-  .contact-des1 span:nth-child(1){
-    color: #ff6666;
-  }
-  .contact-des1 span:nth-child(2){
-    color: #3cf;
-;
-  }
-  .contact-des1 span:nth-child(3){
-    color: #99FF99;
-  }
-  .contact-des1 span:nth-child(4){
-    color: #FFFFCC;
-  }
-  .contact-des2{
-    margin-top: 40px;
-    font-size: 18px;
-    color: #fff;
-    text-align: center;
+    .contact-des1{
+      margin: 0 auto;
+      display: flex;
+      display: flex;
+      justify-content: space-between;
+      font-size: 22px;
+      span:nth-child(1){
+        color: #ff6666;
+      }
+      span:nth-child(2){
+        color: #3cf;
+      }
+      span:nth-child(3){
+        color: #99FF99;
+      }
+      span:nth-child(4){
+        color: #FFFFCC;
+      }
+    }
+    .contact-des2{
+      margin-top: 40px;
+      font-size: 18px;
+      color: #fff;
+      text-align: center;
+    }
   }
 
   @media screen and (max-width: 768px) {
     .contact-container{
       top: 100px;
       max-width: 250px;
-    }
-    .contact-des1{
-      font-size: 18px;
-    }
-    .contact-des2{
-      margin-top: 50px;
-      font-size: 14px;
+      .contact-des1{
+        font-size: 18px;
+      }
+      .contact-des2{
+        margin-top: 50px;
+        font-size: 14px;
+      }
     }
   }
 
@@ -119,23 +118,23 @@ export default {
   @media screen and (min-width: 1500px){
     .contact-container{
       max-width: 400px;
-    }
-    .contact-des1{
-      margin: 0 auto;
-      margin-top: 100px;
-      font-size: 28px;
-    }
-    .contact-des2{
-      font-size: 20px;
-      line-height: 30px;
-    }
-    .contact-type a{
-      position: relative;
-      margin: 0 20px;
-      width: 50px;
-      height: 50px;
-      border: 1px solid #FFF;
-      border-radius: 50%;
+      .contact-des1{
+        margin: 0 auto;
+        margin-top: 100px;
+        font-size: 28px;
+      }
+      .contact-des2{
+        font-size: 20px;
+        line-height: 30px;
+      }
+      .contact-type a{
+        position: relative;
+        margin: 0 20px;
+        width: 50px;
+        height: 50px;
+        border: 1px solid #FFF;
+        border-radius: 50%;
+      }
     }
   }
 </style>
